96df808416d05c40fecdad9397ad970e83db8b57
julkarzunayed <zunayedjulkar@gmail.com>
2025-03-14 21:34:05 +0600

    added navbar and categories buttons

---
+++ script/nav.js
@@ -0,0 +1,25 @@
+
+function loadCategory(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then(res => res.json())
+    .then(data => displayCategory(data.categories) )
+}
+
+function displayCategory (categories){
+    const buttonContainer = document.getElementById("but-container")
+    for(let category of categories){
+        // console.log(category)
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+        <button 
+            class="bg-gray-200 text-gray-500 rounded-md py-2 px-5 font-medium text-lg">
+                    ${category.category}
+        </button>
+        `
+        buttonContainer.appendChild(btnDiv)
+    }
+    // console.log(categories)
+
+}
+
+loadCategory()
